7c8e47421a37019b04020c186021fef7150d4b36
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 15:47:33 +0100

    focus different locator

--- packages/janus-trainer-e2e/tests/configuration.spec.ts
+++ packages/janus-trainer-e2e/tests/configuration.spec.ts
@@ -18,8 +18,8 @@ test.describe('Configuration page', () => {
     //   selector.removeAttribute('disabled');
     // });
 
-    // await page.locator('#holiday-date-picker-start').fill('03.10.2024');
-    await page.getByRole('textbox').nth(0).focus();
+    await page.locator('#holiday-date-picker-start').focus();
+    // await page.getByRole('textbox').nth(0).focus();
     await page.keyboard.type('03.10.2024');
 
     // await page.evaluate(() => {
@@ -27,13 +27,17 @@ test.describe('Configuration page', () => {
     //   selector.removeAttribute('readonly');
     //   selector.removeAttribute('disabled');
     // });
-    await page.getByRole('textbox').nth(1).focus();
+    // await page.getByRole('textbox').nth(1).focus();
+    await page.locator('#holiday-date-picker-end').focus();
     await page.keyboard.type('03.10.2024');
 
     // await page.locator('input#holiday-date-picker-end').fill('03.10.2024');
 
     // the testIds do not work on CI. Also, the getByRole does not work on CI.
-    await page.getByTestId('holiday-text-field-description').focus();
+    await page
+      .getByTestId('holiday-text-field-description')
+      .getByLabel('Beschreibung')
+      .focus();
     await page.keyboard.type('Tag der Dt. Einheit');
 
     // await page
