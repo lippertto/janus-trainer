60d9cba9c3258409bc96d9cf2b77938aa0eb3410
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 14:52:57 +0100

    use German locale

--- packages/janus-trainer-e2e/playwright.config.ts
+++ packages/janus-trainer-e2e/playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     bypassCSP: true,
+    locale: 'de-DE',
   },
 
   /* Configure projects for major browsers */
