b717d9136fc744503e42d15aa5aafba30ea62a26
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 17:23:58 +0100

    reduce test code

--- packages/janus-trainer-e2e/tests/configuration.spec.ts
+++ packages/janus-trainer-e2e/tests/configuration.spec.ts
@@ -9,6 +9,9 @@ test.describe('Configuration page', () => {
   test.use({ storageState: 'playwright/.auth/admin.json' });
 
   test('enter a new holiday', async () => {
+    // we need to configure firefox with having pointer capabilities because MUI will not allow the text fields to be editable
+    // in the default scenario on the server.
+    // https://github.com/mui/mui-x/pull/5684
     const browser = await firefox.launch({
       firefoxUserPrefs: {
         'ui.primaryPointerCapabilities': 0x02 | 0x04,
@@ -20,22 +23,12 @@ test.describe('Configuration page', () => {
     await page.goto('/configure');
     await page.getByTestId('add-holiday-button').click();
 
-    // await page.evaluate(() => {
-    //   const selector = document.querySelector('#holiday-date-picker-start');
-    //   selector.removeAttribute('readonly');
-    //   selector.removeAttribute('disabled');
-    // });
+    await page.locator('#holiday-date-picker-start').fill('03.10.2024');
+    await page.locator('#holiday-date-picker-end').fill('03.10.2024');
+    // await page.getByRole('textbox').nth(0).focus();
 
-    await page.getByRole('textbox').nth(0).focus();
-    await expect(page.getByRole('textbox').nth(0)).toBeEditable();
-    // await page.locator('#holiday-date-picker-start:visible')
-    await page.keyboard.type('03.10.2024');
     // await page.keyboard.type('03.10.2024');
-
-    await page.getByRole('textbox').nth(1).focus();
-    await expect(page.getByRole('textbox').nth(1)).toBeEditable();
-
-    await page.keyboard.type('03.10.2024');
+    // await page.keyboard.type('03.10.2024');
 
     // await page.locator('input#holiday-date-picker-end').fill('03.10.2024');
 
@@ -50,15 +43,15 @@ test.describe('Configuration page', () => {
         .getByLabel('Beschreibung'),
     ).toBeEditable();
 
-    await page.keyboard.type('Tag der Dt. Einheit');
-
-    // await page
-    //   .getByTestId('holiday-text-field-description')
-    //   .getByLabel('Beschreibung')
-    //   .fill('Tag der Dt. Einheit');
+    await page
+      .getByTestId('holiday-text-field-description')
+      .getByLabel('Beschreibung')
+      .fill('Tag der Dt. Einheit');
 
     await page.getByTestId('holiday-button-submit').click();
 
     await expect(page.getByText('Tag der dt. Einheit')).toBeVisible();
+
+    browser.close();
   });
 });
