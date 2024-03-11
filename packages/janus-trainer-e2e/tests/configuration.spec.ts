d2e805bfd2ede085db9188245c346410db37c6b8
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 15:12:54 +0100

    use nth

--- packages/janus-trainer-e2e/tests/configuration.spec.ts
+++ packages/janus-trainer-e2e/tests/configuration.spec.ts
@@ -19,14 +19,16 @@ test.describe('Configuration page', () => {
     // });
 
     // await page.locator('#holiday-date-picker-start').fill('03.10.2024');
-    await page.getByRole('textbox')[0].fill('03.10.2024');
+    await page.getByRole('textbox').nth(0).fill('03.10.2024');
 
-    await page.evaluate(() => {
-      const selector = document.querySelector('input#holiday-date-picker-end');
-      selector.removeAttribute('readonly');
-      selector.removeAttribute('disabled');
-    });
-    await page.locator('input#holiday-date-picker-end').fill('03.10.2024');
+    // await page.evaluate(() => {
+    //   const selector = document.querySelector('input#holiday-date-picker-end');
+    //   selector.removeAttribute('readonly');
+    //   selector.removeAttribute('disabled');
+    // });
+    await page.getByRole('textbox').nth(1).fill('03.10.2024');
+
+    // await page.locator('input#holiday-date-picker-end').fill('03.10.2024');
 
     // the testIds do not work on CI. Also, the getByRole does not work on CI.
     await page.getByTestId('holiday-text-field-description').click();
