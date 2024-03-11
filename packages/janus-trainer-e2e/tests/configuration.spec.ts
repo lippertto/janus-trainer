b17bd854b4b4effb5d34eae4c41d35f405dae1f4
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 14:11:40 +0100

    remove readonly attribute

--- packages/janus-trainer-e2e/tests/configuration.spec.ts
+++ packages/janus-trainer-e2e/tests/configuration.spec.ts
@@ -12,20 +12,19 @@ test.describe('Configuration page', () => {
     await page.goto('/configure');
     await page.getByTestId('add-holiday-button').click();
 
-    // await page.locator('#holiday-date-picker-start').click();
-    await page.locator('#holiday-date-picker-start').fill('03.10.2024');
-    // await page.evaluate(() => {
-    //   const selector = document.querySelector('#holiday-date-picker-start');
-    //   selector.setAttribute('readonly', undefined);
-    // });
+    await page.evaluate(() => {
+      const selector = document.querySelector(
+        'input#holiday-date-picker-start',
+      );
+      selector.removeAttribute('readonly');
+    });
+
     // await page.getByRole('textbox', { name: 'Start' }).fill('03.10.2024');
     // await page.locator('#holiday-date-picker-end').focus;
-    // await page.evaluate(() => {
-    //   const selector = document.querySelector('#holiday-date-picker-end');
-    //   selector.setAttribute('readonly', 'false');
-    // });
 
-    await page.locator('#holiday-date-picker-end').fill('03.10.2024');
+    await page.locator('input#holiday-date-picker-start').fill('03.10.2024');
+
+    await page.locator('input#holiday-date-picker-end').fill('03.10.2024');
 
     // the testIds do not work on CI. Also, the getByRole does not work on CI.
     await page.getByTestId('holiday-text-field-description').click();
