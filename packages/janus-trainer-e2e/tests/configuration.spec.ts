d27063d1ac6812d10768a058dd65b82a0f615990
Tobias Lippert <tobias.lippert@codecentric.de>
2024-03-11 12:51:34 +0100

    fill description before dates

--- packages/janus-trainer-e2e/tests/configuration.spec.ts
+++ packages/janus-trainer-e2e/tests/configuration.spec.ts
@@ -13,14 +13,15 @@ test.describe('Configuration page', () => {
     await page.getByTestId('add-holiday-button').click();
 
     // the testIds do not work on CI. Also, the getByRole does not work on CI.
-    await page.locator('#holiday-date-picker-start').focus();
-    await page.locator('#holiday-date-picker-start').fill('03.10.2024');
-    await page.locator('#holiday-date-picker-end').focus();
-    await page.locator('#holiday-date-picker-end').fill('03.10.2024');
     await page
       .getByTestId('holiday-text-field-description')
       .getByLabel('Beschreibung')
       .fill('Tag der Dt. Einheit');
+
+    await page.locator('#holiday-date-picker-start').focus();
+    await page.locator('#holiday-date-picker-start').fill('03.10.2024');
+    await page.locator('#holiday-date-picker-end').focus;
+    await page.locator('#holiday-date-picker-end').fill('03.10.2024');
     await page.getByTestId('holiday-button-submit').click();
 
     await expect(page.getByText('Tag der dt. Einheit')).toBeVisible();
