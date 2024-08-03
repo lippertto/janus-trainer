3f24eae6442fab2ad2863c4dc84af259c456212b
Tobias Lippert <tobias.lippert@codecentric.de>
2024-08-03 20:14:49 +0200

    feat(tos): new version

--- app/TermsAcceptance.tsx
+++ app/TermsAcceptance.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { JanusSession } from '@/lib/auth';
 
@@ -21,24 +21,45 @@ import { API_USERS } from '@/lib/routes';
 import { showError } from '@/lib/notifications';
 import { CircularProgress } from '@mui/material';
 import { useSession } from 'next-auth/react';
-
-const CURRENT_TERMS_VERSION = '2024-07-13';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 function TermsDialog(props: { tosData: string, handleAccept: () => void }) {
   const [accepted, setAccepted] = React.useState<boolean>(false);
   const [clicked, setClicked] = React.useState<boolean>(false);
   return <Dialog open={true}>
-    <DialogTitle>Nutzungsbedingungen</DialogTitle>
+    <DialogTitle>Allgemeine Geschäftsbedingungen</DialogTitle>
     <DialogContent>
       <Box maxHeight={'80%'}>
         <Stack>
           <Markdown children={props.tosData}
                     components={{
-                      h1: ({ children }) => (<Typography variant={'h1'} children={children} />),
-                      h2: ({ children }) => (<Typography variant={'h2'} children={children} />),
-                      h3: ({ children }) => (<Typography variant={'h3'} children={children} />),
-                      h4: ({ children }) => (<Typography variant={'h4'} children={children} />),
-                      p: ({ children }) => (<Typography children={children} />),
+                      h1: ({ children }) => (<Typography sx={{ mt: 1 }} variant={'h1'} children={children} />),
+                      h2: ({ children }) => (<Typography sx={{ mt: 1 }} variant={'h2'} children={children} />),
+                      h3: ({ children }) => (<Typography sx={{ mt: 1 }} variant={'h3'} children={children} />),
+                      h4: ({ children }) => (<Typography sx={{ mt: 1 }} variant={'h4'} children={children} />),
+                      h5: ({ children }) => (<Typography sx={{ mt: 1 }} variant={'h5'} children={children} />),
+                      p: ({ children }) => (<Typography sx={{ mt: 1 }} children={children} />),
+                      ol: ({ children }) => (<List sx={{
+                        listStyleType: 'decimal',
+                        mt: 2,
+                        pl: 2,
+                        '& .MuiListItem-root': {
+                          display: 'list-item',
+                        },
+                      }}>{children}</List>),
+                      ul: ({ children }) => (<List sx={{
+                        listStyleType: 'disc',
+                        mt: 2,
+                        pl: 2,
+                        '& .MuiListItem-root': {
+                          display: 'list-item',
+                        },
+                      }}>{children}</List>),
+                      li: ({ children, ...props }) => (
+                        <ListItem sx={{ m: 0, p: 0, ml: 2 }} disableGutters><ListItemText
+                          sx={{ pl: 0.25 }}>{children}</ListItemText></ListItem>),
                     }}
           />
           <FormGroup>
@@ -95,7 +116,7 @@ function TermsAcceptanceContents(props: { session: JanusSession }) {
     },
   });
 
-  if (user.termsAcceptedVersion === CURRENT_TERMS_VERSION) {
+  if (user.termsAcceptedVersion === termsVersion) {
     return <></>;
   }
 
