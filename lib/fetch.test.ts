dbfadf9cf242bdfd4e9f3dd80ff0181c5291f957
Tobias Lippert <tobias.lippert@codecentric.de>
2024-08-03 19:23:22 +0200

    test: add fetch tests

---
+++ lib/fetch.test.ts
@@ -0,0 +1,23 @@
+import { buildQueryString } from '@/lib/fetch';
+
+describe("fetchSingleEntity", () => {
+  test("empty components result in empty string", () => {
+    const result = buildQueryString([]);
+    expect(result).toBe("")
+  })
+
+  test("one component has only question mark", () => {
+    const result = buildQueryString(["a=b"]);
+    expect(result).toBe("?a=b")
+  });
+
+  test("second component is joined with ampersand", () => {
+    const result = buildQueryString(["a=b", "c=d"]);
+    expect(result).toBe("?a=b&c=d")
+  });
+
+  test("three components have two ampersands", () => {
+    const result = buildQueryString(["a=b", "c=d", "e=f"]);
+    expect(result).toBe("?a=b&c=d&e=f")
+  });
+});
