5eed46203fe6903bbb4e05544e8f7505dc575d99
Tobias Lippert <tobias.lippert@codecentric.de>
2024-08-03 20:14:37 +0200

    fix(tos): Fix acceptance for ToS

--- app/api/users/[id]/patch.test.ts
+++ app/api/users/[id]/patch.test.ts
@@ -3,13 +3,13 @@ import { patchRequestToUpdateData } from './patch';
 
 
 describe("user patch", () => {
-  test("removes whitespaces when converting request", async () => {
+  test("removes whitespaces when converting request", () => {
     // GIVEN
     const request = new UserPatchRequest(
       {iban: "a b c", },
     );
     // WHEN
-    const result = await patchRequestToUpdateData(request);
+    const result = patchRequestToUpdateData(request);
     // THEN
     expect(result.iban).not.toMatch(/\s/)
   })
