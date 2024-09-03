943cbc58fd8b3569c00e688ecfd8c7a5a871e350
Tobias Lippert <tobias.lippert@codecentric.de>
2024-09-03 08:47:02 +0200

    fix(app/analyze): Make columsn sortable

--- app/analyze/yearly-totals/YearlyTotalsTable.tsx
+++ app/analyze/yearly-totals/YearlyTotalsTable.tsx
@@ -1,7 +1,14 @@
 import { YearlyTotalDto } from '@/lib/dto';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { centsToHumanReadable } from '@/lib/formatters';
 import Box from '@mui/material/Box';
+
+function currencyFormatter(value: number) {
+  return new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+  }).format(value);
+}
 
 function buildColumns(): GridColDef[] {
   const compensationCentsWidth = 100; // allows to show 1000,00 €
@@ -13,19 +20,47 @@ function buildColumns(): GridColDef[] {
     { field: 'trainingCountQ3', headerName: 'Q3 T', width: trainingCountWidth },
     { field: 'trainingCountQ4', headerName: 'Q4 T', width: trainingCountWidth },
     { field: 'trainingCountTotal', headerName: 'Σ T', width: trainingCountWidth },
-    { field: 'compensationCentsQ1', headerName: 'Q1 €', width: compensationCentsWidth, valueGetter: centsToHumanReadable },
-    { field: 'compensationCentsQ2', headerName: 'Q2 €', width: compensationCentsWidth, valueGetter: centsToHumanReadable },
-    { field: 'compensationCentsQ3', headerName: 'Q3 €', width: compensationCentsWidth, valueGetter: centsToHumanReadable },
-    { field: 'compensationCentsQ4', headerName: 'Q4 €', width: compensationCentsWidth, valueGetter: centsToHumanReadable },
     {
-      field: 'compensationCentsTotal', headerName: 'Σ €', width: compensationCentsWidth, valueGetter: centsToHumanReadable,
+      field: 'compensationCentsQ1',
+      headerName: 'Q1 €',
+      width: compensationCentsWidth,
+      valueGetter: (value) => value / 100,
+      valueFormatter: currencyFormatter,
+    },
+    {
+      field: 'compensationCentsQ2',
+      headerName: 'Q2 €',
+      width: compensationCentsWidth,
+      valueGetter: (value) => value / 100,
+      valueFormatter: currencyFormatter,
+    },
+    {
+      field: 'compensationCentsQ3',
+      headerName: 'Q3 €',
+      width: compensationCentsWidth,
+      valueGetter: (value) => value / 100,
+      valueFormatter: currencyFormatter,
+    },
+    {
+      field: 'compensationCentsQ4',
+      headerName: 'Q4 €',
+      width: compensationCentsWidth,
+      valueGetter: (value) => value / 100,
+      valueFormatter: currencyFormatter,
+    },
+    {
+      field: 'compensationCentsTotal', headerName: 'Σ €',
+      type: 'number',
+      width: compensationCentsWidth,
+      valueGetter: (value) => value / 100,
+      valueFormatter: currencyFormatter,
       cellClassName: (params) => {
         if (params.row.compensationCentsTotal < 3000 * 100) {
           return '';
         } else {
-          return 'yearly-total-warning'
+          return 'yearly-total-warning';
         }
-      }
+      },
     },
   ];
 }
