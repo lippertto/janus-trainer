64fa7e38436657e2f3382a4a978874bc7fd39fa6
Tobias Lippert <tobias.lippert@fastmail.com>
2026-04-24 16:02:47 +0200

    test(api): Run the tests in-process

--- api-tests/vite.config.ts
+++ api-tests/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   test: {
     include: ['api-tests/*.test.ts'],
     testTimeout: 50_000,
+    globalSetup: ['./api-tests/global-setup.ts'],
   },
   resolve: {
     alias: [
